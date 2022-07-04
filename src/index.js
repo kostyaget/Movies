5c1fa004540b584f4581ae264e0a22395e1f5b23
kostyaget <progogyd@gmail.com>
2022-07-04 16:27:10 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from 'components/App';
-// import reportWebVitals from './reportWebVitals';
 import './index.css';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/goit-react-hw-05-movies/">
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
